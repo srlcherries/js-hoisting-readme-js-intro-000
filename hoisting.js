8f428493792625fe9573210e150b5f04cb4ebb25
Sherry Lin <sherrylin222@gmail.com>
2018-05-09 03:41:40 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,18 +1,18 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
+  return (lyric);
 }
-
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
+  
+  var thisIsCrazy; 
+    thisIsCrazy();
+    thisIsCrazy = function (){
     console.log("hey!!!")
   }
 }
@@ -20,11 +20,11 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
-  sayMy();
-
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
+  sayMy();
 }
+
+
